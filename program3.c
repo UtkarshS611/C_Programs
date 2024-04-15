708865e9ab7b49e2f7f779df9dabf563399f2d9d
Utkarsh Singh <156522686+UtkarshS611@users.noreply.github.com>
2024-04-16 00:11:36 +0530

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 // #include <conio.h>
-#define PI 3.1415926
-
-float myfunc(float r) {  
-float a;
-a=2*PI*r;
-return(a);
-}
-
-float myfunc1(float r) {
-float s;
-s=PI*r*r;
-return(s);
-}
+#include <math.h>
 
 int main() {
     float radius, circum, area;
-    float myfunc(float radius);
+    float PI = 3.1415926;
 
     // clrscr();
 
     printf("Radius: ");
     scanf("%f", &radius);
-    circum = myfunc(radius);
-    area = myfunc1(radius);
+    circum = 2*PI*radius;
+    // area = PI*radius*radius;
+    area = PI*(pow(radius, 2));
     printf("\nCircumference is %f", circum);
     printf("\nArea is %f", area);
 
